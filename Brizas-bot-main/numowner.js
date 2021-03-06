0f34fbcc1c0be20525b9382bb517c0f9d742c2fb
migbutest <miguelzinhobu@gmail.com>
2021-03-06 11:10:56 -0300

    first commit

---
+++ Brizas-bot-main/numowner.js
@@ -0,0 +1,5 @@
+const ownerNumber = ["+12072044684@s.whatsapp.net"]
+const OriginalOwner = '12072044684'
+
+exports.OriginalOwner = OriginalOwner
+exports.ownerNumber = ownerNumber
---
+++ Brizas-bot-main/src/pack.js
@@ -0,0 +1,12 @@
+const pack = (prefix) => {
+return `╔════ ❯ 𝓹𝓪𝓬𝓴 𝓭𝓮 𝓱𝓮𝓷𝓽𝓪𝓲 𝓮 𝓸𝓷𝓵𝔂𝓯𝓪𝓷𝓼 𝓭𝓸 𝓶𝓮𝓰𝓪 ❮ ══════════
+║ ❯ *FOTOS DE HENTAI 720MB:* https://seulink.net/7XMddz
+║ ❯ *PRINCESS STARLING(ONLY FANS)8.36GB:*  https://seulink.net/IEQtAom
+║ ❯ *ELLES CLUB 28.68GB:* https://seulink.net/NyRZ
+║ ❯ *COCONUTKITTY(ONLY FANS)7.69GB:* https://seulink.net/7XMddz
+║ ❯ *Belle delphine:* https://seulink.net/S5NM
+║ ❯ *Love Lilah:* https://seulink.net/nzDQ1
+║ ❯ *Hidori:* https://seulink.net/RlV2omEE
+╚═══════════════════════════`
+}
+exports.pack = pack
